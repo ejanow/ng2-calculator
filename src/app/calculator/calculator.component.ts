64a241b37ee40c78a9d8881953cf5723d6092ef7
Eric Janowski <ejanow@gmail.com>
2016-09-24 23:20:11 -0500

    added keyboard event handler

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -27,22 +27,23 @@ class Calculator {
     this.clear();
   }
 
-  processOp(op: Operator): void { 
-    if (op === Operator.Clear) 
+  processOp(op: Operator): void {
+    if (op === Operator.Clear) {
       return this.clear();
+    }
 
     switch( this.op ) {
       case Operator.Plus:  this.val += this.old; break;
       case Operator.Minus: this.val += this.old; break;
       case Operator.Times: this.val *= this.old; break;
-      case Operator.Divide: this.val /= this.old; break;
+      case Operator.Divide: this.val = this.old / this.val; break;
       default: break;
     }
     this.isClean = true;
     this.op = op;
   }
 
-  processNum(i: number){
+  processNum(i: number) {
     if (this.isClean) {
       this.old = this.val;
       this.val = i;
@@ -52,6 +53,40 @@ class Calculator {
     }
 
     this.isClean = false;
+  }
+
+  keyboardEventHandler(event:KeyboardEvent) {
+    switch( event.keyCode ) {
+        case 96:  
+        case 48: this.processNum(0); break;
+        case 97:
+        case 49: this.processNum(1); break;
+        case 98: 
+        case 50: this.processNum(2); break;
+        case 99: 
+        case 51: this.processNum(3); break;
+        case 100: 
+        case 52: this.processNum(4); break;
+        case 101:
+        case 53: this.processNum(5); break;
+        case 102:
+        case 54: this.processNum(6); break;
+        case 103:
+        case 55: this.processNum(7); break;
+        case 104:
+        case 56: this.processNum(8); break;
+        case 105:
+        case 57: this.processNum(9); break;
+        case 189:
+        case 109: this.processOp(Operator.Minus); break;
+        case 27:
+        case 8: this.processOp(Operator.Clear); break;
+        case 106: this.processOp(Operator.Times); break;
+        case 107: this.processOp(Operator.Plus); break;
+        case 13: this.processOp(Operator.Equals); break;
+
+        default: break;
+    }
   }
 
 }
@@ -66,11 +101,18 @@ export class CalculatorComponent implements OnInit {
   public calc: Calculator;
   public op = Operator;
 
-  constructor() { 
+  constructor() {
     this.calc = new Calculator();
   }
 
   ngOnInit() {
+    let self = this;
+    setTimeout( () => {
+      document.addEventListener('keydown', (event) => { 
+        self.calc.keyboardEventHandler(event)
+        });
+    }, 0);
+
   }
 
 }
